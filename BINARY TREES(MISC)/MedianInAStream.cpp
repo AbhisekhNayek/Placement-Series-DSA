02d073c1c4fbb423362ef668cab5042b783c0015
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-01-24 15:50:58 +0530

    BINARY TREE MISC

---
+++ BINARY TREES(MISC)/MedianInAStream.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+vector<int> findMedian(vector<int> &arr, int n){
+
+
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    vector<int> ans;
+
+    for(int i=0; i<n; i++){
+
+        if(maxHeap.empty() || arr[i] < maxHeap.top()){
+
+            maxHeap.push(arr[i]);
+        }
+
+        else{
+            minHeap.push(arr[i]);
+        }
+
+        if(maxHeap.size() > minHeap.size()+1){
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+
+        else if(minHeap.size() > maxHeap.size()+1){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+
+        if(maxHeap.size() < minHeap.size()){
+            ans.push_back(minHeap.top());
+        }
+
+        else if(maxHeap.size() > minHeap.size()){
+            ans.push_back(maxHeap.top());
+        }
+
+        else{
+            int temp = (minHeap.top()+maxHeap.top())/2;
+            ans.push_back(temp);
+        }
+
+    }
+    return ans;
+}
